8289e6078f376fb23ca4f86792e0add1d47fbbec
David Thomas <dave@davespace.co.uk>
2013-02-01 21:46:46 +0000

    Use -o to overwrite images.
    This means I no longer need to delete the entire output directory before regenerating the HTML format output.

--- kit.sh
+++ kit.sh
@@ -1,3 +1,2 @@
-rm -rf TheGreatEscape 
 sna2skool.py -H -c TheGreatEscape.ctl -M TheGreatEscape.prof TheGreatEscape.z80 > TheGreatEscape.skool 
-skool2html.py -H TheGreatEscape.skool
+skool2html.py -H -o TheGreatEscape.skool
