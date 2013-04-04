e1e7dfa9beaecc869cfb6879e4e9b6ffbe80c91e
David Thomas <dave@davespace.co.uk>
2013-04-04 14:21:33 +0100

    Replace install.sh and kit.sh with a Makefile. Build stuff in 'build'.

--- reverse/install.sh
+++
@@ -1,1 +0,0 @@
-sudo cp thegreatescape.py /Library/Python/2.7/site-packages/skoolkit/
--- reverse/kit.sh
+++
@@ -1,2 +0,0 @@
-sna2skool.py -H -c TheGreatEscape.ctl -M TheGreatEscape.prof TheGreatEscape.z80 > TheGreatEscape.skool 
-skool2html.py -H -o TheGreatEscape.skool
